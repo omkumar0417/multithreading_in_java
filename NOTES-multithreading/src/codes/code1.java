0a0677dc56f3e66ba10f813acf92657e5c764ace
omkumar0417 <omkumar201206@gmail.com>
2025-03-15 13:47:19 +0530

    commit

---
+++ NOTES-multithreading/src/codes/code1.java
@@ -0,0 +1,8 @@
+package codes;
+
+public class code1 {
+	public static void main(String[] args) {
+        System.out.println("Hello world !");
+        System.out.println(Thread.currentThread().getName());// prints main 
+    }
+}
